947abc18503606156320b490ad667aabf98f33b4
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2020-09-14 15:33:49 +0200

    muh

--- neo4j/datadog_checks/neo4j/neo4j.py
+++ neo4j/datadog_checks/neo4j/neo4j.py
@@ -39,6 +39,11 @@ class Neo4jCheck(PrometheusCheck):
         # Determine if metrics.namespaces.enabled is set in the target Neo4j instance
         # Finding this dynamically lets users roll out this feature without interrupting their metrics,
         # as well as monitoring database fleets with mixed values for this setting.
+        is_namespaced = True
+        print(f'fofofofo {metrics}')
+        for metric in metrics:
+            print(f'fififi: {metric}')
+
         self._check_namespaced_metrics(metrics, config)
 
     def _check_namespaced_metrics(self, metrics, config):
